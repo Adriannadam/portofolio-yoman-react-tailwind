300b8dc4887f1d96c1140b8f556566b62866754b
unknown <adrianadam120@gmail.com>
2025-08-13 15:39:27 +0700

    feat: Add responsive design for hero section

--- src/data/project.js
+++ src/data/project.js
@@ -1,12 +1,12 @@
 export const projectsData = [
   {
     id: 1,
-    title: "Proyek 1: Portofolio Website",
+    title: "Proyek 1: Staycation",
     description:
-      "Website Portofolio simple yang dibangun dengan React dan state management dasar.",
-    imageUrl: "/1.png",
-    tags: ["React", "JavaScript", "CSS"],
-    codeUrl: "https://github.com/Adriannadam/portofolio-yoman-react-tailwind",
-    liveUrl: "#", // Jika ada live demo
+      "A full-stack hotel booking web application built with the MERN (MongoDB, Express, React, Node.js) stack.",
+    imageUrl: "/2.png",
+    tags: ["MongoDB", "JavaScript", "React", "NodeJS"],
+    codeUrl: "https://github.com/Adriannadam/staycation",
+    liveUrl: "https://staycation-xi-ruby.vercel.app/",
   },
 ];
